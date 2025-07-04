c2f6e25c7ab80d659adaadeda71c96af572d8efa
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-04 15:25:09 +0300

    typeScript-todo

---
+++ ToDoApp/src/App.tsx
@@ -0,0 +1,11 @@
+import TodoCreate from './components/TodoCreate';
+
+function App() {
+  return (
+    <div>
+      <TodoCreate />
+    </div>
+  );
+}
+
+export default App;
---
+++ ToDoApp/src/components/TodoCreate.tsx
@@ -0,0 +1,10 @@
+function TodoCreate() {
+  return (
+    <div className="todo-create">
+      <input type="text" />
+      <button>Create</button>
+    </div>
+  );
+}
+
+export default TodoCreate;
